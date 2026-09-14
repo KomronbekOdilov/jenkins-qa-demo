748fb588fcc4cee863fb0aae2bbbb8968919dd1e
KomronbekOdilov <odilovkomron9019@gmail.com>
2026-09-15 01:10:27 +0500

    Add pytest tests

---
+++ tests/test_demo.py
@@ -0,0 +1,11 @@
+def test_addition():
+    assert 2 + 2 == 4
+
+
+def test_string():
+    assert "Jenkins" in "Jenkins QA Demo"
+
+
+def test_list():
+    numbers = [1, 2, 3]
+    assert len(numbers) == 3
