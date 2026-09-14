8f38ee28e8aee4f351d1809989937686b257052b
KomronbekOdilov <odilovkomron9019@gmail.com>
2026-09-15 02:36:19 +0500

    Added markers for tests

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,11 +1,19 @@
+import pytest
+
+
+@pytest.mark.smoke
 def test_addition():
-    assert 2 + 2 == 4
+    actual = 2 + 2
+    expected = 4
+    assert actual == expected
 
 
+@pytest.mark.smoke
 def test_string():
     assert "Jenkins" in "Jenkins QA Demo"
 
 
+@pytest.mark.regression
 def test_list():
     numbers = [1, 2, 3]
     assert len(numbers) == 3
